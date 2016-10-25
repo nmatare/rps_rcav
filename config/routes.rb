b9bb3fe207709f9579ae4f59a6ead778cadc4bfd
nmatare <nmatare@chicagobooth.edu>
2016-10-25 17:13:20 -0500

    outcome not working; but app logic is sound

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get("/rock", :controller => "sessions", :action => "rock")
+  get("/paper", :controller => "sessions", :action => "paper")
+  get("/scissors", :controller => "sessions", :action => "scissors")
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
